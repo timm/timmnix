a9304c116e01d0acd96367ff8fc217bd65f74c5b
Timothy Menzies <timm@Timothys-MacBook-Air.local>
2015-08-20 23:27:19 -0400

    fixing minor typo

--- brews.sh
+++ brews.sh
@@ -10,6 +10,10 @@ sudo -H pip install --user --install-option="--prefix=" -U scikit-learn
 echo "need full xcode installed"
 brew install macvim
 brew linkapps macvim
+#brew google-chrome
+#brew vlc
+#brew bartender
+#brew iterm2
 #macvim medeealy
 #skim
 
