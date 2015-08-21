ddda1cf7fe0266aacdcfc037b99ef57bf440b259
Timothy Menzies <timm@Timothys-MacBook-Air.local>
2015-08-20 22:20:01 -0400

    fixing minor typo

--- brews.sh
+++ brews.sh
@@ -3,4 +3,6 @@ brew install emacs --cocoa
 brew linkapps emacs
 brew install git markdown pandoc wget gawk \
      imagemagick ispell gnuplot
-
+sudo easy_install pip
+pip install "requests[security]"
+sudo -H pip install --user --install-option="--prefix=" -U scikit-learn
