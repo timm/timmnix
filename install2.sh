ab16ef3f9b207934fc844ecd3bcb24fa3174f75c
Tim Menzies <tim@menzies.us>
2017-01-29 00:24:17 -0500

    saving

---
+++ install2.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python-pip python-dev build-essential 
+sudo pip install --upgrade pip 
+
+sudo add-apt-repository ppa:ricotz/docky
+
+sudo apt-get update
+sudo apt-get install plank
