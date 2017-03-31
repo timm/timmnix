aaf1fa9214310b914dabbdd799374abc650dbf61
Tim Menzies <tim.menzies@gmail.com>
2017-03-30 20:39:19 -0400

    saving

--- install2.sh
+++ install2.sh
@@ -43,3 +43,5 @@ sudo  apt install ./keyring.deb
 sudo  echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
 sudo  apt update
 sudo  apt install i3
+rm .keyring.deb
+
