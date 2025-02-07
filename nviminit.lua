a3e90e68f525fd1835c5be01fc9b9de3c2d71283
timm <timm@ieee.org>
2025-02-06 21:43:07 -0500

    as

---
+++ nviminit.lua
@@ -0,0 +1,35 @@
+-- Learn about Neovim's lua api
+-- https://neovim.io/doc/user/lua-guide.html
+
+vim.o.number = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.smartcase = true
+vim.o.ignorecase = true
+vim.o.wrap = false
+vim.o.hlsearch = false
+vim.o.signcolumn = 'yes'
+
+-- Space as the leader key
+vim.g.mapleader = vim.keycode('<Space>')
+
+-- Basic clipboard interaction
+vim.keymap.set({'n', 'x', 'o'}, 'gy', '"+y', {desc = 'Copy to clipboard'})
+vim.keymap.set({'n', 'x', 'o'}, 'gp', '"+p', {desc = 'Paste clipboard text'})
+
+-- Command shortcuts
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', {desc = 'Save file'})
+vim.keymap.set('n', '<leader>q', '<cmd>quitall<cr>', {desc = 'Exit vim'})
+
+vim.cmd.colorscheme('retrobox')
+
+require('mini.completion').setup({})
+
+require('mini.files').setup({})
+vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open()<cr>', {desc = 'File explorer'})
+
+require('mini.pick').setup({})
+vim.keymap.set('n', '<leader><space>', '<cmd>Pick buffers<cr>', {desc = 'Search open files'})
+vim.keymap.set('n', '<leader>ff', '<cmd>Pick files<cr>', {desc = 'Search all files'})
+vim.keymap.set('n', '<leader>fh', '<cmd>Pick help<cr>', {desc = 'Search help tags'})
+
